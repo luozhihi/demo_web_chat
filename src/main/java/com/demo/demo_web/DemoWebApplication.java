f70d8fa6a7a35c9c4b3d8d7f3453d0ec4183994d
luozhihui_vendor <luozhihui_vendor@sensetime.com>
2019-02-26 16:10:54 +0800

    解决数据源bug

--- src/main/java/com/demo/demo_web/DemoWebApplication.java
+++ src/main/java/com/demo/demo_web/DemoWebApplication.java
@@ -1,9 +1,11 @@
 package com.demo.demo_web;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.demo.demo_web.dao")
 public class DemoWebApplication {
 
 	public static void main(String[] args) {
